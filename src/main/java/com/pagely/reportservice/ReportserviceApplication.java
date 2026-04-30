6dca00ff9b871858a1b42d21d2e7aa4510badc68
jsh9057 <jsh9057@gmail.com>
2026-05-01 03:14:22 +0900

    chore: feignclient 설정 추가

--- src/main/java/com/pagely/reportservice/ReportserviceApplication.java
+++ src/main/java/com/pagely/reportservice/ReportserviceApplication.java
@@ -2,12 +2,14 @@ package com.pagely.reportservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
+@EnableFeignClients
 @SpringBootApplication
 public class ReportserviceApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ReportserviceApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(ReportserviceApplication.class, args);
+    }
 
 }
