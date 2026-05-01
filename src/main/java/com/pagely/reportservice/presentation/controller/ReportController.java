b794463d47c70a019e7f17001726043d7e2d8dc0
jsh9057 <jsh9057@gmail.com>
2026-05-02 03:46:30 +0900

    refactor: 정렬 조건 최신순으로 고정, 공통 모듈의 인증된 헤더를 가져오도록 수정
    - 정렬 조건을 임의로 조작하여 의도치않게 다른 정보를 제공할 수 있어, 초기에는 제어가능한 최신순으로 강제

--- src/main/java/com/pagely/reportservice/presentation/controller/ReportController.java
+++ src/main/java/com/pagely/reportservice/presentation/controller/ReportController.java
@@ -18,12 +18,10 @@ import java.util.UUID;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
-import org.springframework.data.domain.Sort;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -48,19 +46,16 @@ public class ReportController {
     @AuthRequired(role = {Role.MASTER, Role.CREATOR, Role.USER})
     @GetMapping
     public ResponseEntity<ApiResponse> getReports(
-            @RequestHeader("X-User-Id") UUID userId,
+            @CurrentUserId UUID userId,
             @RequestParam(required = false) ReadScope readScope,
             @RequestParam(defaultValue = "0") int page,
-            @RequestParam(defaultValue = "10") int size,
-            @RequestParam(defaultValue = "createdAt") String sortBy,
-            @RequestParam(defaultValue = "DESC") Sort.Direction direction
+            @RequestParam(defaultValue = "10") int size
     ) {
-        Sort sort = Sort.by(direction, sortBy);
-        Pageable pageable = PageRequest.of(page, size).toPageable(sort);
+        Pageable pageable = PageRequest.of(page, size).toPageable();
         GetReportsQuery query = new GetReportsQuery(userId, readScope);
 
         Page<ReportResult> result = reportQueryService.getReports(query, pageable);
-        
+
         return ApiResponse.ok(result, GetReportsResponseDto::from);
     }
 }
